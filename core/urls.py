41cf46ab80ba56c96f049b2ae69d28e2395a4574
AptYam <anikronjon.swe@gmail.com>
2021-07-09 19:24:41 +0600

    Install fresh project

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+admin.site.site_title = "Yam's site"
+admin.site.site_header = "AptYam's Dashboard"
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
