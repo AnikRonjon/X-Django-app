a083352d616257cdb98c2b108923f88697361496
AptYam <anikronjon.swe@gmail.com>
2021-07-10 10:50:04 +0600

    StartApp School

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ admin.site.site_title = "Yam's site"
 admin.site.site_header = "AptYam's Dashboard"
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('school.urls', namespace='school')),
 ]
